a87d727d0e4e7f473b27bbb0c1877bfde1d3db6f
Otto Mozale <66283993+OttozaoJooj@users.noreply.github.com>
2026-04-21 21:39:10 -0300

    template react js

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Button} from 'primereact/button';
+import {Card} from 'primereact/card';
+
+/*https://api.thingspeak.com/channels/3334856/feeds.json?api_key=NS5N1TSW1ULKFX3O&results=30 */
+function App() {
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
